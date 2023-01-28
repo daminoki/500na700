4b3f3bfa24453c7f01b68476e7fa074ba9870274
Daria Andreeva <dd1556@mail.ru>
2023-01-28 16:21:56 +0300

    initial commit

---
+++ dist/assets/js/main.js
@@ -0,0 +1,49 @@
+// swiper
+const swiper = new Swiper('.swiper', {
+    loop: true,
+    spaceBetween: 20,
+    slidesPerView: 1.5,
+    centeredSlides: true,
+
+    navigation: {
+        nextEl: '.gallery__button-next',
+        prevEl: '.gallery__button-prev',
+    },
+
+    breakpoints: {
+        0: {
+            slidesPerView: 1,
+            spaceBetween: 40
+        },
+        600: {
+            slidesPerView: 1.5,
+            spaceBetween: 20,
+        }
+    }
+});
+
+// accordion
+const questionsList = document.querySelectorAll(".questions__question");
+
+questionsList.forEach(question => {
+    question.addEventListener("click", () => handleClick(question))
+})
+
+function handleClick(question) {
+    const answer = question.nextElementSibling;
+    const wrapper = answer.parentNode;
+
+    answer.classList.toggle("questions__answer_visible");
+    question.classList.toggle("questions__question_active");
+    wrapper.classList.toggle("questions__wrapper_active");
+
+    questionsList.forEach(item => {
+        if (item !== question) {
+            const answerItem = item.nextElementSibling;
+            const wrapperItem = answerItem.parentNode;
+            answerItem.classList.remove("questions__answer_visible");
+            item.classList.remove("questions__question_active");
+            wrapperItem.classList.remove("questions__wrapper_active");
+        }
+    })
+}
---
+++ gulpfile.js
@@ -0,0 +1,151 @@
+"use.strict"
+
+const { src, dest } = require("gulp")
+const gulp = require("gulp")
+const autoprefixer = require("gulp-autoprefixer")
+const cssbeautify = require("gulp-cssbeautify")
+const removeComments = require("gulp-strip-css-comments")
+const rename = require("gulp-rename")
+const sass = require("gulp-sass")(require("sass"))
+const cssnano = require("gulp-cssnano")
+const uglify = require("gulp-uglify")
+const plumber = require("gulp-plumber")
+const panini = require("panini")
+const imagemin = require("gulp-imagemin")
+const del = require("del")
+const notify = require("gulp-notify")
+const browserSync = require("browser-sync").create()
+const rigger = require("gulp-rigger")
+
+const srcPath = "src/"
+const distPath = "dist/"
+
+const path = {
+    build: {
+        html: distPath,
+        css: distPath + "assets/css/",
+        js: distPath + "assets/js/",
+        images: distPath + "assets/images/",
+    },
+    src: {
+        html: srcPath + "*.html",
+        css: srcPath + "assets/scss/style.scss",
+        js: srcPath + "assets/js/main.js",
+        images: srcPath + "assets/images/**/*.{jpeg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+    },
+    watch: {
+        html: srcPath + "**/*.html",
+        css: srcPath + "assets/scss/**/*.scss",
+        js: srcPath + "assets/js/**/*.js",
+        images: srcPath + "assets/images/**/*.{jpeg,png,svg,gif,ico,webp,webmanifest,xml,json}",
+    },
+    clean: "./" + distPath
+}
+
+function serve() {
+    browserSync.init({
+        server: {
+            baseDir: "./" + distPath
+        }
+    })
+}
+
+function html() {
+    return src(path.src.html, { base: srcPath })
+        .pipe(plumber())
+        .pipe(dest(path.build.html))
+        .pipe(browserSync.reload({stream:true}))
+}
+
+function css() {
+    return src(path.src.css, { base: srcPath + "assets/scss/" })
+        .pipe(plumber({
+            errorHandler: function(err) {
+                notify.onError({
+                    title: "SCSS Error",
+                    message: "Error: <%= error.message %>"
+                })(err)
+                this.emit("end")
+            }
+        }))
+        .pipe(sass())
+        .pipe(autoprefixer())
+        .pipe(cssbeautify())
+        .pipe(dest(path.build.css))
+        .pipe(cssnano({
+            zindex: false,
+            discardComments: {
+                removeAll: true
+            }
+        }))
+        .pipe(removeComments())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".css"
+        }))
+        .pipe(dest(path.build.css))
+        .pipe(browserSync.reload({stream:true}))
+}
+
+function js() {
+    return src(path.src.js, { base: srcPath + "assets/js/" })
+        .pipe(plumber({
+            errorHandler: function(err) {
+                notify.onError({
+                    title: "JS Error",
+                    message: "Error: <%= error.message %>"
+                })(err)
+                this.emit("end")
+            }
+        }))
+        .pipe(rigger())
+        .pipe(dest(path.build.js))
+        .pipe(uglify())
+        .pipe(rename({
+            suffix: ".min",
+            extname: ".js"
+        }))
+        .pipe(dest(path.build.js))
+        .pipe(browserSync.reload({stream:true}))
+}
+
+
+function images() {
+    return src(path.src.images, { base: srcPath + "assets/images/" })
+        .pipe(imagemin([
+            imagemin.gifsicle({interlaced: true}),
+            imagemin.mozjpeg({quality: 80, progressive: true}),
+            imagemin.optipng({optimizationLevel: 5}),
+            imagemin.svgo({
+                plugins: [
+                    {removeViewBox: true},
+                    {cleanupIDs: false}
+                ]
+            })
+        ]))
+        .pipe(dest(path.build.images))
+        .pipe(browserSync.reload({stream:true}))
+}
+
+function clean() {
+    return del(path.clean)
+}
+
+function watchFiles() {
+    gulp.watch([path.watch.html], html)
+    gulp.watch([path.watch.css], css)
+    gulp.watch([path.watch.js], js)
+    gulp.watch([path.watch.images], images)
+}
+
+const build = gulp.series(clean, gulp.parallel(html, css, js, images))
+const watch = gulp.parallel(build, watchFiles, serve)
+
+exports.html = html
+exports.css = css
+exports.js = js
+exports.images = images
+exports.clean = clean
+exports.build = build
+exports.watch = watch
+exports.default = watch
