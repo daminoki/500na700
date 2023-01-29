fabf8bc2daf422a757beb259eca7b96df0fd6b95
Daria Andreeva <dd1556@mail.ru>
2023-01-29 12:15:05 +0300

    add tel mask using vanilla js

--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -63,3 +63,53 @@ menuCloseBtn.addEventListener("click", () => {
     overlay.classList.remove("mobile-nav__overlay_opened");
     mainPage.classList.remove("page_mobile-menu-opened");
 })
+
+// tel input mask 
+const telInput = document.querySelector("#tel");
+
+const prefixNumber = (str) => {
+  if (str === "7") {
+    return "7 (";
+  }
+  if (str === "8") {
+    return "8 (";
+  }
+  if (str === "9") {
+    return "7 (9";
+  }
+  return "7 (";
+};
+
+telInput.addEventListener("input", (e) => {
+    const value = telInput.value.replace(/\D+/g, "");
+    const numberLength = 11;
+  
+    let result;
+    if (telInput.value.includes("+8") || telInput.value[0] === "8") {
+      result = "";
+    } else {
+      result = "+";
+    }
+  
+    for (let i = 0; i < value.length && i < numberLength; i++) {
+      switch (i) {
+        case 0:
+          result += prefixNumber(value[i]);
+          continue;
+        case 4:
+          result += ") ";
+          break;
+        case 7:
+          result += "-";
+          break;
+        case 9:
+          result += "-";
+          break;
+        default:
+          break;
+      }
+      result += value[i];
+    }
+
+    telInput.value = result;
+  });
